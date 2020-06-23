8fd91eed2d50f431286feda8afdbe672c5d3d748
Marcelo <marcelovqvd@gmail.com>
2020-06-23 14:52:47 -0300

    baseURL and layout

--- Readme.md
+++ Readme.md
@@ -2,8 +2,35 @@
 
 Projeto desenvolvidos na aulas do Bootcamp GOStack da Rocketseat em 2020.
 
+## Iniciar
 react-native start --reset-cache (para evitar bug de inicialização)
 react-native run-android  
 
 API para integrações:
 https://github.com/Marcelovqvd/Rocketseat2020-Back-end-com-Node.js
+
+Axios
+Opções para conexão do dispotivio com localhost:
+- iOS com Emulador: localhost;
+- iOS com aparelho físico: IP da máquina;
+- Android com Emulador: localhost com abd reverse;
+- Android com Emulador do Android Studio: 10.0.2.2
+- Android com Emulador Genymotion: 10.0.3.2
+- Android com aparelho físico: IP da máquina
+
+Utilizando adb reverse:
+Redirecionar portas
+adb reverse tcp:3333 tcp:3333
+
+Utilizando IP
+Buscar o IP da máquina: ifconfig
+wlp9s0 (wifi) ou enp4s0 (cabo) 
+baseURL: 'http://ip:3333',
+ex. baseURL: 'http://192.168.0.12:3333',
+
+adb devices
+Depois da o comando adb -s <número do device> reverse tcp:3333 tcp:3333
+
+## Console.log
+Para visualizar os logs - chacoalhar o Android e clicar em Debug.
+Inspec no browser que abrirá
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -18,11 +39,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#7519c1',
   },
-  title: {
-    color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
+  project: {
+    color: '#FFF',
+    fontSize: 30,
   },
 });
