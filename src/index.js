57bb62404664585585987ee61a25fb8a27c71bda
Marcelo <marcelovqvd@gmail.com>
2020-06-23 09:46:56 -0300

    initial layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7519c1',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
